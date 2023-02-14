4099fd907d39cbe4d137fd6d7bd49ff5351e685e
leyo <ardileyo@gmail.com>
2023-02-14 23:55:36 +0700

    repush

--- components/section/content-experience.tsx
+++ components/section/content-experience.tsx
@@ -25,8 +25,9 @@ export const ContentExperience = ({ title, children }: any) => {
     };
 
     return (
-        <Container maxW='full'>
-            <Box maxW={{ base: '70vw', }} animation="1s ease-out 0s 1 slideInFromBottom" >
+        <Container maxW='full'  >
+            <Box maxW={{ base: '70vw', }}
+                animation="1s ease-out 0s 1 slideInFromBottom">
                 <Slider {...settings} >
                     <Box p={4} >
                         <Card p={{ base: 2, md: 4 }} size='sm'
@@ -69,10 +70,45 @@ export const ContentExperience = ({ title, children }: any) => {
                             </CardBody>
                         </Card>
                     </Box>
-                    <Box p={4}>
-                        <Card p={3} size='sm'
+                    <Box p={4} >
+                        <Card p={{ base: 2, md: 4 }} size='sm'
                             boxShadow='lg' borderRadius='xl'
                             bg={useColorModeValue('#F7FAFC', '#262d35')} >
+                            <CardHeader display={'flex'} mb={2}>
+                                <Avatar mr={2} name='Ardi Leo' loading="lazy" src='https://www.ocbcnisp.com/asset/media/OCBC/Favicon/Android/favicon-96x96.png' />
+                                <Flex flex={1} flexWrap='wrap' gap={2} direction={{ base: 'column', md: 'row' }}>
+                                    <Box flex={1}>
+                                        <Heading size='md' textTransform='uppercase' letterSpacing={1}>
+                                            PT OCBC NISP
+                                        </Heading>
+                                        <Text>Jun 2021 - Current</Text>
+                                    </Box>
+                                    <Text >IT Software Engineer</Text>
+                                </Flex>
+                            </CardHeader>
+                            <CardBody >
+                                <Stack divider={<StackDivider />} spacing='4'>
+                                    <Box>
+                                        <Heading size='xs' textTransform='uppercase'>
+                                            Summary
+                                        </Heading>
+                                        <UnorderedList pt='2' fontSize='sm' mx={5}>
+                                            <ListItem>Lorem ipsum dolor sit amet</ListItem>
+                                            <ListItem>Consectetur adipiscing elit</ListItem>
+                                            <ListItem>Integer molestie lorem at massa</ListItem>
+                                            <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+                                        </UnorderedList>
+                                    </Box>
+                                    <Box>
+                                        <Heading size='xs' textTransform='uppercase'>
+                                            Tools
+                                        </Heading>
+                                        <Text pt='2' fontSize='sm'>
+                                            Check out the overview of your clients.
+                                        </Text>
+                                    </Box>
+                                </Stack>
+                            </CardBody>
                         </Card>
                     </Box>
 
